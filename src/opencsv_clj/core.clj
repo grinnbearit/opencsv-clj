325a6226e5ccc18f44041b1f8e3668048bb6b4f7
Sidhant Godiwala <sidhant@infinitelybeta.com>
2010-07-29 15:29:25 +0530

    Added documentation

--- src/opencsv_clj/core.clj
+++ src/opencsv_clj/core.clj
@@ -1,8 +1,11 @@
 (ns opencsv-clj.core
+  #^{:author "Sidhant Godiwala"
+     :doc "This file defines a clojure wrapper for the opencsv library with laziness built in."}
   (:import [au.com.bytecode.opencsv CSVReader])
   (:require [clojure.java.io :as io]))
 
 (defn- read-csv [path]
+  "Reads a csv file and generates a lazy sequence of rows"
   (let [buffer (CSVReader. (io/reader path))]
     (lazy-seq
      (loop [res []]
@@ -11,8 +14,10 @@
          res)))))
 
 (defn- parse-csv [csv-seq]
+  "Converts a lazy sequence of rows to a lazy sequence of maps"
   (let [header (first csv-seq)]
     (map #(zipmap header %) (rest csv-seq))))
 
 (defn parse [path]
+  "Converts a csv file to a lazy sequence of maps of the values where the keys are the forst row"
   (parse-csv (read-csv path)))
