b49d528988c46667d3910238437c3d61d57907a1
Sidhant Godiwala <sidhant@infinitelybeta.com>
2010-07-29 14:35:45 +0530

    removed dependancy for contrib

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject opencsv-clj "1.0.0-SNAPSHOT"
   :description "clojure wrapper for opencsv"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [net.sf.opencsv/opencsv "2.0"]]
   :dev-dependencies [[swank-clojure "1.2.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]])
