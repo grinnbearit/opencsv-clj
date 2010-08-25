8053d0b4b65eaf06238e698ee82776605b3c044f
Sidhant Godiwala <sidhant.godiwala@infinitelybeta.com>
2010-08-25 19:42:19 +0530

    Add function to write a csv file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject opencsv-clj "1.0.0-SNAPSHOT"
   :description "clojure wrapper for opencsv"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [net.sf.opencsv/opencsv "2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]])
--- src/opencsv_clj/core.clj
+++ src/opencsv_clj/core.clj
@@ -1,7 +1,7 @@
 (ns opencsv-clj.core
   #^{:author "Sidhant Godiwala"
      :doc "This file defines a clojure wrapper for the opencsv library with laziness built in."}
-  (:import [au.com.bytecode.opencsv CSVReader])
+  (:import [au.com.bytecode.opencsv CSVReader CSVWriter])
   (:require [clojure.java.io :as io]))
 
 (defn- read-csv [path]
@@ -21,3 +21,18 @@
 (defn parse [path]
   "Converts a csv file to a lazy sequence of maps of the values where the keys are the items in the header row"
   (parse-csv (read-csv path)))
+
+(defn dump
+  "Dumps a sequence of maps to a file given by path, pass a header to specify the order and columns to be written"
+  ([path csv-seq]
+     (let [header (keys (first csv-seq))]
+       (dump path csv-seq header)))
+  ([path csv-seq header]
+     (with-open [writer (io/writer path)]
+       (let [csv-writer (CSVWriter. writer)]
+         (.. csv-writer  (writeNext (into-array (map str header))))
+         (doseq [entry (map  (comp into-array
+                                   (fn [csv-entry] (map (comp str #(get csv-entry %))
+                                                       header)))
+                             csv-seq)]
+           (.. csv-writer (writeNext entry)))))))
