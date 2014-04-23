2957c76d7c96b71f318912b4df89220e13689207
Sidhant Godiwala <grinnbearit@liara.local>
2014-04-23 16:11:51 +0530

    slash and burn

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject opencsv-clj "1.2.1"
+(defproject opencsv-clj "2.0.0"
   :description "clojure wrapper for opencsv"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [net.sf.opencsv/opencsv "2.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [net.sf.opencsv/opencsv "2.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
