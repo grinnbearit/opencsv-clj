6d04e56bb7b2f1a4b28a11acc9ce80285cf77d73
Ryan Sundberg <ryan.sundberg@gmail.com>
2016-02-17 17:36:48 -0800

    Bump version to 2.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject opencsv-clj "2.0.2"
+(defproject opencsv-clj "2.1.0"
   :description "clojure wrapper for opencsv"
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
